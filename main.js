157f4d005966fe8902d5ab40cf3451cd7f9a52f4
Grebbes <Grebbes@gmail.com>
2025-11-19 18:12:50 +0100

    created needed variables for game, changed name of 1-2 ids in index.html file

---
+++ main.js
@@ -0,0 +1,19 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventry = ["stick"];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xp-Text");
+const healthText = document.querySelector("#health-Text");
+const goldText = document.querySelector("#gold-Text");
+const monsterStats = document.querySelector("#monsterstats");
+const monsterHealthText = document.querySelector("#monster-Health");
+const monsterNameText = document.querySelector("#monster-Name")
