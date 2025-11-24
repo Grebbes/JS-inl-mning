3caac7e2f77757b6170ac2dd8e664142c39248f8
Grebbes <Grebbes@gmail.com>
2025-11-24 19:29:33 +0100

    cleaned up change background code made it easier to reuse also made a restart function

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ let fighting;
 let monsterHealth;
 let inventry = ["stick"];
 let healthPotion = 10;
+let image = 0;
 
-let background = document.querySelector(".fullscreen");
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -38,28 +38,23 @@ const monsters = [
 
 button2.addEventListener("click", () => {
   let changeBackground = document.body;
-  changeBackground.classList.toggle("fullscreen2");
+  changeBackground.style.backgroundImage = `url(Images/image${image = 2}.png)`;
+  
+function restartGame() {
+  location.reload();
+}
 
-  if (changeBackground.classList.contains("fullscreen2")) {
+  if (image = 2) {
     text.innerText =
       "You turn around and leave the Town to it's fate, letting it burn to the ground and leaving the villagers without a home.";
     button1.innerText = "Restart";
     button2.innerText = "Restart";
-  } else {
-    text.innerText =
-      "Greeting traveler, our town is in dire need of help! would you help us please?";
-    button1.innerText = "Help the town";
-    button2.innerText = "Turn around and leave them";
-  }
+    button2.addEventListener("click", () => {
+      restartGame();
+    });
+    button1.addEventListener("click", () => {
+      restartGame();
+    });
+  } 
 });
 
-button1.addEventListener("click", () => {
-  let changeBackground = document.body;
-  if (changeBackground.classList.contains("fullscreen2")) {
-    changeBackground.classList.toggle("fullscreen2");
-    text.innerText =
-      "Greeting traveler, our town is in dire need of help! would you help us please?";
-    button1.innerText = "Help the town";
-    button2.innerText = "Turn around and leave them";
-  }
-});
