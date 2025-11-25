c2a2ba3d22beea5bb509b05b8ba76918f8d3c696
Grebbes <Grebbes@gmail.com>
2025-11-25 09:34:48 +0100

    small changes

--- main.js
+++ main.js
@@ -36,80 +36,92 @@ const monsters = [
   { name: "Orc warlord", level: 20, health: 200 },
 ];
 const locations = [
-  {name: "Town Square",
-   "button text": ["Enter the armory", "Enter the potion shop", "Hunt for orcs", "Raid the orc encampment"],
-  //  "button function": [goArmory, goPotionShop, goHunt, goRaid],
-   text: "You step into the town square. The usual bustle is muted, villagers move about with hurried steps and worried eyes, whispering among themselves. Nearby, two shops stand out, both look like they might prove useful in gathering some supplies and armour." 
+  {
+    name: "Town Square",
+    "button text": [
+      "Enter the armory",
+      "Enter the potion shop",
+      "Hunt for orcs",
+      "Raid the orc encampment",
+    ],
+    //  "button function": [goArmory, goPotionShop, goHunt, goRaid],
+    text: "You step into the town square. The usual bustle is muted, villagers move about with hurried steps and worried eyes, whispering among themselves. Nearby, two shops stand out, both look like they might prove useful in gathering some supplies and armour.",
   },
   {
     name: "Armory",
-    "button text": ["Buy weapon(30gold)", "Buy armour(10gold)", "Go back to town square"],
+    "button text": [
+      "Buy weapon(30gold)",
+      "Buy armour(10gold)",
+      "Go back to town square",
+    ],
     // "button function": [buyWeapon, buyArmour, goTown],
-    text: "You step into the armory, and immediately a sharp, assessing gaze cuts through the dim light. An old man stands behind the counter—scarred, weathered, and clearly no stranger to battle. His eyes narrow slightly as he studies you.Well then,’ he rasps, ‘you here to buy some armor, or are you just taking in the scenery?"
+    text: "You step into the armory, and immediately a sharp, assessing gaze cuts through the dim light. An old man stands behind the counter—scarred, weathered, and clearly no stranger to battle. His eyes narrow slightly as he studies you.Well then,’ he rasps, ‘you here to buy some armor, or are you just taking in the scenery?",
   },
   {
     name: "Potion Shop",
     "button text": ["Buy health potion", "Go back to town square"],
     // "button function": [buyPotion, goTown],
-    text: "You step into the potion shop, greeted by swirling scents of herbs and faintly glowing vials. A soft, knowing gaze meets yours, an older woman stands behind the counter. There is something otherworldly about her, as if she sees more then she lets on.‘Welcome, dear one,’ she murmurs, her voice warm and strangely enchanting. ‘Seeking a potion… or simply wandering where curiosity leads you?’"
+    text: "You step into the potion shop, greeted by swirling scents of herbs and faintly glowing vials. A soft, knowing gaze meets yours, an older woman stands behind the counter. There is something otherworldly about her, as if she sees more then she lets on.‘Welcome, dear one,’ she murmurs, her voice warm and strangely enchanting. ‘Seeking a potion… or simply wandering where curiosity leads you?’",
   },
   {
     name: "Hunting",
-    "button text": ["Fight rabid beast", "Fight Orc raider", "Fight Orc shaman", "Go back to town"],
+    "button text": [
+      "Fight rabid beast",
+      "Fight Orc raider",
+      "Fight Orc shaman",
+      "Go back to town",
+    ],
     // "button function": [fightBeast, fightRaider, fightShaman, goTown],
-    text: "Through the twisted trees, you catch sight of an orc encampment. Flames leap from crude fires, casting shadows that twitch like living things. Inside the camp you spot a rabid beast snarlig at the edge, fast but weak, and orc raider sharpening his axe, dangerous but beatable, and a dark orc shaman chanting by a smoky brazier, powerful and deadly... what do you do?"
+    text: "Through the twisted trees, you catch sight of an orc encampment. Flames leap from crude fires, casting shadows that twitch like living things. Inside the camp you spot a rabid beast snarlig at the edge, fast but weak, and orc raider sharpening his axe, dangerous but beatable, and a dark orc shaman chanting by a smoky brazier, powerful and deadly... what do you do?",
   },
   {
     name: "Raid",
     "button text": ["Fight the Orc warlord", "Go back to town"],
     // "button function": [fightWarlord, goTown],
-    text: "The chamber opens before you, dark and cavernous, torches sputtering along stone walls, the air heavy with the scent of battle and steel. At the center stands the orc warlord, fierce, battle, hardened, and radiating deadly strength. Every muscle tensed, every scar a story of conquest, he glares at you. This is the greatest challenge yet. Will you face him?"
+    text: "The chamber opens before you, dark and cavernous, torches sputtering along stone walls, the air heavy with the scent of battle and steel. At the center stands the orc warlord, fierce, battle, hardened, and radiating deadly strength. Every muscle tensed, every scar a story of conquest, he glares at you. This is the greatest challenge yet. Will you face him?",
   },
   {
     name: "fight",
     "button text": ["Attack", "Dodge", "Drink potion", "Run"],
     // "button function": [attack, dodge, drinkPotion, run],
-    text: "You are fighting the orcs."
+    text: "You are fighting the orcs.",
   },
   {
     name: "Kill monster",
     "button text": ["Go to town square", "Go to town square"],
     // "button function": [goTown, goTown],
-    text: "With a final strike, the enemy falls. Silence fills the clearing, your victory is clear but the fight is far from over."
+    text: "With a final strike, the enemy falls. Silence fills the clearing, your victory is clear but the fight is far from over.",
   },
   {
     name: "lose",
     "button text": ["Replay", "Replay"],
     "button function": [restartGame, restartGame],
-    text: "Darkness rushes in as your strength fades. The world blurs, and the last thing you feel is the weight of your defeat. The battle is over... and you have fallen."
+    text: "Darkness rushes in as your strength fades. The world blurs, and the last thing you feel is the weight of your defeat. The battle is over... and you have fallen.",
   },
   {
     name: "win",
     "button text": ["Replay", "Replay"],
     "button function": [restartGame, restartGame],
-    text: "With a final, resounding strike, the orc warlord collapses, his fierce glare fading at last. Silence descends over the stronghold, broken only by the echo of your victory. You have faced the greatest threat and emerged triumphant, the battle is won, and the village and realm is safe... for now."
-  }
-  
+    text: "With a final, resounding strike, the orc warlord collapses, his fierce glare fading at last. Silence descends over the stronghold, broken only by the echo of your victory. You have faced the greatest threat and emerged triumphant, the battle is won, and the village and realm is safe... for now.",
+  },
 ];
 
 function restartGame() {
   location.reload();
 }
 
-
-button2.addEventListener("click", () => {
+function dontHelp() {
   let changeBackground = document.body;
-  changeBackground.style.backgroundImage = `url(Images/image${image = 2}.png)`;
-  
-
+  changeBackground.style.backgroundImage = `url(Images/image${(image = 2)}.png)`;
 
   if (image === 2) {
     text.innerText =
-      "You turn around and leave the Town to it's fate, letting it burn to the ground and leaving the villagers without a home.";
+      "You turn away, leaving the town to its fate. Fire consume the streets, smoke curling into the sky, and the villagers are left to face ruin without a home. The cries of the desperate fade behind you as you walk away.";
     button1.innerText = "Restart";
     button2.innerText = "Restart";
     button2.onclick = restartGame;
     button1.onclick = restartGame;
-  } 
-});
+  }
+}
 
+button2.onclick = dontHelp;
